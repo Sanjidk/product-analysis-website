17865a67711efbab63f4dde10ebe5d2cb9fbdbca
Sanjid <sanjidk24@gmail.com>
2022-04-05 00:28:56 +0600

    Question Answer Done

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,9 +1,36 @@
+import './Blogs.css';
 import React from 'react';
+import { Row, Card, Col } from 'react-bootstrap';
 
 const Blogs = () => {
   return (
     <div>
-            <h1>Hello from Blogs</h1>
+      <Row xs={1} md={1} className="g-4">
+    <Col>
+      <Card className='m-5 shadow-lg'>
+        <Card.Body>
+          <Card.Title className='text-primary'>Contex API</Card.Title>
+          <Card.Text className='text-start blog-para'>
+          The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.
+          <br/>
+          React.createContext() is all you need. It returns a consumer and a provider. Provider is a component that as it's names suggests provides the state to its children. It will hold the "store" and be the parent of all the components that might need that store. Consumer as it so happens is a component that consumes and uses the state.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+      <Card className='m-5 shadow-lg'>
+        <Card.Body>
+          <Card.Title className='text-primary'>Semantic Tag</Card.Title>
+          <Card.Text className='text-start blog-para'>
+          Semantic HTML5 addresses this shortcoming by defining specific tags to indicate clearly what role is played by the content those tags contain. That explicit information helps robots/crawlers like Google and Bing to better understand which content is important, which is a subsidiary, which is for navigation, and so on.
+          <br/>
+          By adding semantic HTML tags to your pages, you provide additional information that helps Google and Bing understand the roles and relative importance of the different parts of your page.
+
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  )
+</Row>
 
     </div>
   );
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,67 @@
 import React from 'react';
+import { CartesianGrid, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
+
+  const data = [
+    {
+        "month": "Mar",
+        "investment": 100,
+        "sell": 80,
+        "revenue": 20
+    },
+    {
+        "month": "Apr",
+        "investment": 200,
+        "sell": 180,
+        "revenue": 80
+    },
+    {
+        "month": "May",
+        "investment": 300,
+        "sell": 250,
+        "revenue": 50
+    },
+    {
+        "month": "Jun",
+        "investment": 500,
+        "sell": 550,
+        "revenue": 50
+    },
+    {
+        "month": "Jul",
+        "investment": 600,
+        "sell": 800,
+        "revenue": 200
+    },
+    {
+        "month": "Aug",
+        "investment": 700,
+        "sell": 970,
+        "revenue": 260
+    }
+  ]
+
   return (
     <div>
-            <h1>Hello from Dashboard</h1>
-
+        <div className='mt-5 ms-3'>
+        <LineChart width={600} height={300} data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
+      <Line dataKey="investment" stroke="#00FF00" />
+      <Line dataKey="sell" stroke="#0000FF" />
+      <Line dataKey="revenue" stroke="#FF0000" />
+      <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+      <XAxis dataKey="month" />
+      <YAxis />
+      <Tooltip />
+    </LineChart>
+      </div>
+      <div>
+        
+      </div>
     </div>
+
+    
+
   );
 };
 
