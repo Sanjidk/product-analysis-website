955ecad4da025efa9c7d10d07222078945acc449
Sanjid <sanjidk24@gmail.com>
2022-04-05 02:50:31 +0600

    home half section complete

--- src/components/Homee/Homee.js
+++ src/components/Homee/Homee.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import canon from './canon1200d.jpg';
+import { Button } from 'react-bootstrap';
 
 const Homee = () => {
   return (
     <div>
-      <h1>Hello from Home</h1>
+      <div>
+        <main className='d-flex mx-5 gap-5 align-items-center'>
+          <section className='text-start'>
+            <h1 className='fw-bold'> DSLR CANON 1200D </h1>
+            <p className='fw-normal'>The dynamic range of the Canon 1200D is just as good as that of the more expensive Canon SLR cameras with an APS-C sensor. It is noticeably better than the dynamic range of a smartphone or compact camera, but a bit lower than the dynamic range of the most modern cameras from Nikon, Olympus or Panasonic.</p>
+            <Button>See More</Button>
+          </section>
+          <section>
+            <img src={canon} alt="" />
+          </section>
+        </main>
+    
+      </div>
+      <div>
+
+      </div>
     </div>
   );
 };
